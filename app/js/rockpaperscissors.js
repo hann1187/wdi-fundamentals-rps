0481fc535ff898873f960e0b846fced3b34fe3a2
hann1187 <lhannum86@gmail.com>
2016-05-18 22:35:20 -0700

    cleaned up code.  took out unnecessary sections/improved syntax

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -24,47 +24,37 @@ function randomPlay() {
   // Write an expression that operates on a variable called `move`
   // If a `move` has a value, your expression should evaluate to that value.
   // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-  var getPlayerMove= function(move) {
-      if ((move === 'rock') || (move === 'paper') || (move ==='scissors' )){
-          return move;
-      }else {
-          getInput();
+  var getPlayerMove = function(move) {
+          return move || getInput();
       }
-    }
 
   // Write an expression that operates on a variable called `move`
   // If a `move` has a value, your expression should evaluate to that value.
   // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-var getComputerMove = function (move) {
-    if ((move ==='rock') || (move ==='paper') || (move==='scissors' )) {
-        return move;
-    }else {
-        return randomPlay();
+var getComputerMove = function() {
+  return randomPlay();
     }
- }
 
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
 var getWinner = function(playerMove, computerMove) {
-    var playerMove = getPlayerMove(move);
-    var computerMove = getComputerMove(move);
     var winner
     if (playerMove===computerMove){
-      winner === "tie";
+      winner = "tie";
     }
     else if (playerMove==="rock" &&computerMove==="scissors"){
-      winner==="Player!";
+      winner = "Player!";
     }
     else if (playerMove==="paper" &&computerMove==="rock"){
-      winner==="Player!";
+      winner = "Player!";
     }
     else if (playerMove==="scissors" &&computerMove=="paper"){
-      winner==="Player!"
+      winner = "Player!";
     }
     else {
-      winner==="Computer!";
+      winner = "Computer!";
     }
     return winner;
     }
@@ -75,29 +65,26 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    while (playerWins <=5 && computerWins <=5) {
-      var playerMove = getPlayerMove(move);
-      var computerMove = getComputerMove(move);
+    while (playerWins < 5 && computerWins < 5) {
+      var playerMove = getPlayerMove();
+      var computerMove = getComputerMove();
       var winner = getWinner(playerMove, computerMove);
-      if (winner=== "player") {
-        console.log("Player Wins!" + "" + "Player chose" + playerMove + "while computer chose" + computerMove);
+      if (winner=== "Player!") {
+        console.log("Player Wins!  Player chose  " + playerMove + "  while computer chose  " + computerMove);
         playerWins += 1;
-        console.log("The score is now" + playerWins + "to" + computerWins + ".");
+        console.log("The score is now  " + playerWins + "  to  " + computerWins + ".");
+        console.log();
     }
-    else if (winner === "computer") {
-        console.log("Computer Wins!" + "" + "Computer chose" + computerMove + "while player chose" + playerMove);
+    else if (winner === "Computer!") {
+        console.log("Computer Wins!  Computer chose  " + computerMove + "  while player chose  " + playerMove);
         computerWins += 1;
-        console.log("The score is now" + playerWins + "to" + computerWins + ".");
+        console.log("The score is now  " + playerWins + "  to  " + computerWins + ".");
+        console.log();
     }
     else {
-        console.log("It's a tie!")
-        computerWins +=1;
-        playerWins +=1;
+        console.log("It's a tie!");
+        console.log();
     }
-    return (playerWins, computerWins);
-      }
-    }
-
-
-
+  }
+    return [playerWins, computerWins];
 }
